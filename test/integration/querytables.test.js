bb2b89f3b63d5bf780f01d6e543278e880c2acaf
Raul Marin <git@rmr.ninja>
2019-07-30 11:03:05 +0200

    WIP: getAffectedTables using EXPLAIN ANALYZE

--- test/integration/querytables.test.js
+++ test/integration/querytables.test.js
@@ -3,6 +3,7 @@
 const assert = require('assert');
 const QueryTables = require('../../lib/querytables');
 const SubstitutionTokens = require('../../lib/utils/substitution_tokens');
+const PSQL = require('cartodb-psql');
 
 describe('QueryTables', function() {
 
@@ -18,26 +19,39 @@ describe('QueryTables', function() {
         };
     }
 
+    function createDBConnection() {
+        const dbParams = require('../test_config').postgres;
+        const dbPoolParams = {};
+        let connection = undefined;
+        assert.doesNotThrow(() => { connection = new PSQL(dbParams, dbPoolParams);});
+        assert.ok(connection);
+        return connection;
+    }
+
     describe('getQueryMetadataModel', function() {
+        const connection = createDBConnection();
+        const params = {};
+        const readOnly = false;
+        before((done) => {
+            connection.query('CREATE TABLE t1(a integer)', params, (err, result) =>{
+                assert.ok(!err, err);
+                done();
+            }, readOnly);
+        });
 
         it('should return a DatabaseTables model', function(done) {
-            const mockConnection = createMockConnection(null, [{
-                dbname: 'dbd',
-                schema_name: 'public',
-                table_name: 't1',
-                updated_at: new Date()
-            }]);
-            QueryTables.getQueryMetadataModel(mockConnection, 'foo-bar-query', function (err, result) {
+            
+            QueryTables.getQueryMetadataModel(connection, 'Select * from t1', function (err, result) {
                 assert.ok(!err, err);
                 assert.ok(result);
-                assert.equal(result.getCacheChannel(), 'dbd:public.t1');
+                assert.equal(result.getCacheChannel(), `${require('../test_config').postgres.dbname}:public.t1`);
                 return done();
             });
         });
 
         const tokens = ['bbox', 'pixel_width', 'pixel_height', 'scale_denominator'];
         tokens.forEach(token => {
-            it('should not call Postgres with token: ' + token, function(done) {
+            xit('should not call Postgres with token: ' + token, function(done) {
                 const mockConnection = createMockConnection(null, [{
                     dbname: 'dbd',
                     schema_name: 'public',
@@ -55,7 +69,7 @@ describe('QueryTables', function() {
             });
         });
 
-        it('should rethrow db errors', function(done) {
+        xit('should rethrow db errors', function(done) {
             const mockConnection = createMockConnection(new Error('foo-bar-error'));
             QueryTables.getQueryMetadataModel(mockConnection, 'foo-bar-query', function (err) {
                 assert.ok(err);
